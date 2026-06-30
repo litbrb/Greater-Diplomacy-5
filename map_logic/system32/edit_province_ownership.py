a549cfad5bd5cb541966c99ffa6de61dd8fe91d8
GitGetGot415 <55815440+GitGetGot415@users.noreply.github.com>
2026-06-29 22:33:13 -0700

    integrated puppets are properly removed if they control no territory

--- map_logic/system32/edit_province_ownership.py
+++ map_logic/system32/edit_province_ownership.py
@@ -60,6 +60,43 @@ def conquer_province(self, province, new_owner):
                                 new_color = get_mixed_core_color(p["cores"])
                                 map_utils.update_single_province_surface(self.cores_map, self.id_map, p["map_color"], new_color)
                                 if self.map_mode == "CORES": self.active_map = self.cores_map
+                                
+                    # --- NEW: Completely remove the puppet from the game ---
+                    # 1. Break puppet link from master
+                    master = self.nation_data[old_owner].get("master", "")
+                    if master and master in self.nation_data:
+                        if old_owner in self.nation_data[master].get("puppets", []):
+                            self.nation_data[master]["puppets"].remove(old_owner)
+                            
+                    # 2. Cleanup references in other nations
+                    for n_id, n_data in list(self.nation_data.items()):
+                        if old_owner in n_data.get("at_war_with", []):
+                            n_data["at_war_with"].remove(old_owner)
+                        if old_owner in n_data.get("allied_with", []):
+                            n_data["allied_with"].remove(old_owner)
+                        if old_owner in n_data.get("puppets", []):
+                            n_data["puppets"].remove(old_owner)
+                        if n_data.get("master") == old_owner:
+                            n_data["master"] = ""
+                            n_data["puppet_type"] = ""
+                            
+                        for dict_key in ["relations", "truces", "diplo_cooldowns", "wargoals", "pending_diplomacy", "draft_lists"]:
+                            if old_owner in n_data.get(dict_key, {}):
+                                del n_data[dict_key][old_owner]
+                                
+                        # Remove messages sent by the dead puppet to avoid ghost notifications
+                        if "inbox" in n_data:
+                            n_data["inbox"] = [msg for msg in n_data["inbox"] if msg.get("sender") != old_owner]
+                                
+                    # Cleanup faction maps
+                    fac = self.nation_data[old_owner].get("faction", "")
+                    if fac and "FACTION_WAR_MAPS" in self.nation_data:
+                        if fac in self.nation_data["FACTION_WAR_MAPS"]:
+                            if old_owner in self.nation_data["FACTION_WAR_MAPS"][fac]:
+                                del self.nation_data["FACTION_WAR_MAPS"][fac][old_owner]
+                                
+                    # 3. Finally delete from nation_data
+                    del self.nation_data[old_owner]
 
 def get_mixed_core_color(cores):
     """Helper function to average the colors of all cores on a tile."""
